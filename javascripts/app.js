e8e8d2cabc62abe347422bc8abd0674e338ecbeb
Ordonez Sebastian <20246@columbushs.com>
2019-11-12 09:39:06 -0500

    commit

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,13 +1,53 @@
-// Rover Object Goes Here.
-// ======================
+var rover = {
+  direction : 'N',
+  x: 0,
+  y: 0,
 
-// ======================
+}
+
 function turnLeft(rover){
   console.log("turnLeft was called!");
+
+  switch(rover.direction){
+    case 'N': 
+      rover.direction = 'W';
+      break;
+    case 'W':
+      rover.direction = 'S';
+      break;
+      case 'S': 
+      rover.direction = 'E';
+      break;
+      case 'E': 
+      rover.direction = 'N';
+      break;
+  }
+
+
 }
 
 function turnRight(rover){
   console.log("turnRight was called!");
+
+switch(rover.direction){
+  case 'N':
+    rover.direction = 'E'
+    break;
+    case 'E':
+    rover.direction = 'S'
+    break;
+    case 'S':
+    rover.direction = 'W'
+    break;
+    case 'W':
+    rover.direction = 'N'
+    break;
+
+}
+
+
+
+
 }
 
 function moveForward(rover){
